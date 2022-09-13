0fdc23cd4a80dcd6036a55bc2b63028cbef4f633
Antonio <adcdenis@gmail.com>
2022-09-13 12:49:28 -0300

    melhorias no login, expira ao fechar navegador, retirado poput de força de senha

--- src/app/dstv/service/auth-service.service.ts
+++ src/app/dstv/service/auth-service.service.ts
@@ -2,7 +2,7 @@ import { Observable, map } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { browserSessionPersistence, getAuth, setPersistence, signInWithEmailAndPassword } from 'firebase/auth';
 
 @Injectable({
     providedIn: 'root',
@@ -44,26 +44,63 @@ export class AuthServiceService {
 
     login(email: string, password: string) {
         const auth = getAuth();
-        signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed in
-                this.user = userCredential.user;
 
-                this.authF.authState.subscribe((user) => {
-                    if (user) {
-                        this.user = user;
-                        //localStorage.setItem('userdstv',JSON.stringify(this.user));
-                        this.router.navigate(['dashboard']);
-                    }
-                });
-            })
-            .catch((error) => {
-                console.log('error: ', error);
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                //localStorage.removeItem('userdstv');
-                window.alert(error.message);
-            });
+        setPersistence(auth, browserSessionPersistence)
+        .then(() => {
+          // Existing and future Auth states are now persisted in the current
+          // session only. Closing the window would clear any existing state even
+          // if a user forgets to sign out.
+          // ...
+          // New sign-in will be persisted with session persistence.
+          signInWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+              // Signed in
+              this.user = userCredential.user;
+
+              this.authF.authState.subscribe((user) => {
+                  if (user) {
+                      this.user = user;
+                      //localStorage.setItem('userdstv',JSON.stringify(this.user));
+                      this.router.navigate(['dashboard']);
+                  }
+              });
+          })
+          .catch((error) => {
+              console.log('error: ', error);
+              const errorCode = error.code;
+              const errorMessage = error.message;
+              //localStorage.removeItem('userdstv');
+              window.alert(error.message);
+          });
+        })
+        .catch((error) => {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
+
+
+
+        // signInWithEmailAndPassword(auth, email, password)
+        //     .then((userCredential) => {
+        //         // Signed in
+        //         this.user = userCredential.user;
+
+        //         this.authF.authState.subscribe((user) => {
+        //             if (user) {
+        //                 this.user = user;
+        //                 //localStorage.setItem('userdstv',JSON.stringify(this.user));
+        //                 this.router.navigate(['dashboard']);
+        //             }
+        //         });
+        //     })
+        //     .catch((error) => {
+        //         console.log('error: ', error);
+        //         const errorCode = error.code;
+        //         const errorMessage = error.message;
+        //         //localStorage.removeItem('userdstv');
+        //         window.alert(error.message);
+        //     });
     }
     logout() {
         this.authF.signOut();
